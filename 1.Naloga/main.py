89dd7dde1e894bcf110ca4ece61a256ba2109e51
mrozma <rozmanmiha96@gmail.com>
2021-02-08 11:14:49 +0100

    firs commitž

---
+++ 1.Naloga/main.py
@@ -0,0 +1,12 @@
+mood = input("What`s your mood today? ")
+
+if mood == "happy":
+    print("It`s great to see you happy!")
+elif mood =="nervous":
+    print("Take a deep breath 3 times.")
+elif mood =="sad":
+    print("Cher up, mate")
+elif mood =="excited":
+    print("It`s nice that you`re excited")
+else:
+    print("I don`t recognize this mood")
